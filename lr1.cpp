5fd3a6b9ad8a6d81e275763c9726cca69c8adb5a
Yan Buinevich <iyanbuinevich799@gmail.com>
2020-10-28 14:31:44 +0300

    finish

--- lr1.cpp
+++ lr1.cpp
@@ -19,5 +19,14 @@ int main()
 		arr2[i] = rand() % 11 - 5;
 		printf_s("%d ", arr2[i]);
 	}
+	printf_s("\nitogovii massiv:\n");
+	for (int i = 0; i < 10; i++) // генерация и вывод третьего массива
+	{
+		if (arr1[i] == arr2[i]) // проверка на одинаковые значения
+			arr3[i] = arr1[i];
+		else
+			arr3[i] = arr1[i] + arr2[i]; // если значения одинаковые то производится сложение
+		printf_s("%d ", arr3[i]);
+	}
 	return 0;
 }

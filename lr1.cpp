8b4739e47a198afbf6e2806afadcfce3334a67fa
Yan Buinevich <iyanbuinevich799@gmail.com>
2020-10-28 14:30:13 +0300

    add 2 arrays generation

--- lr1.cpp
+++ lr1.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include <iostream>
+#include <malloc.h>
+#include <math.h>
+#include <time.h>
+
+int main()
+{
+	int arr1[10], arr2[10], arr3[10]; // объявление массивов
+	srand(time(NULL)); // присвоение ключей генерации случайных чисел в зависимости от времени
+	printf_s("pervii massiv: \n");
+	for (int i = 0; i < 10; i++) // генерация и вывод первого массива
+	{
+		arr1[i] = rand() % 11 - 5;
+		printf_s("%d ", arr1[i]);
+	}
+	printf_s("\nvtoroi massiv: \n");
+	for (int i = 0; i < 10; i++) // генерация и вывод второго массива
+	{
+		arr2[i] = rand() % 11 - 5;
+		printf_s("%d ", arr2[i]);
+	}
+	return 0;
+}
